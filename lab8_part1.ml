9ff64d28e5dd1564ee16894b9ee3fce542ae3fc0
Jonny Zhang <jonathan_zhang@college.harvard.edu>
2020-02-29 11:33:08 -0800

    done

--- lab8_part1.ml
+++ lab8_part1.ml
@@ -96,30 +96,43 @@ module MakeInterval (Endpoint : ORDERED_TYPE) =
        `high` inclusive. If `low` is greater than `high`, then the
        interval is empty. *)
     let create (low : Endpoint.t) (high : Endpoint.t) : interval =
-      failwith "create not implemented"
+      if Endpoint.compare low high > 0 then Empty
+      else Interval(low, high)
 
     (* is_empty intvl -- Returns true if and only if `intvl` is
        empty *)
     let is_empty (intvl : interval) : bool =
-      failwith "is_empty not implemented"
+      match intvl with
+        | Empty -> true      
+        | Interval _ -> false
 
     (* contains intvl x -- Returns true if and only if the value `x`
        is contained within `intvl` *)
     let contains (intvl : interval) (x : Endpoint.t) : bool =
-      failwith "contains not implemented"
+      match intvl with
+      | Empty -> false
+      | Interval (low, high) ->
+         Endpoint.compare x low >= 0 && Endpoint.compare x high <= 0
 
     (* intersect intvl1 intvl2 -- Returns the intersection of `intvl1`
        and `intvl2` *)
     let intersect (intvl1 : interval) (intvl2 : interval) : interval =
-      failwith "intersect not implemented"
-    end ;;
+      let ordered x y = if Endpoint.compare x y <= 0 then x, y else y, x in
+        match intvl1, intvl2 with      
+        | Empty, _      
+        | _, Empty -> Empty      
+        | Interval (low1, high1), Interval (low2, high2) ->
+          let (_, low), (high, _)  = ordered low1 low2, ordered high1 high2 in
+             create low high
+  end ;;
 
 (*......................................................................
 Exercise 1B: Using the completed functor above, instantiate an integer
 interval module.
 ......................................................................*)
-
-module IntInterval = struct end ;;
+module IntInterval =  MakeInterval (struct type t = int 
+                                   let compare = Stdlib.compare 
+                                   end) ;;
 
 (*......................................................................
 Exercise 1C: Using your newly created integer interval module, create
@@ -127,10 +140,9 @@ two non-empty intervals named `intvl1` and `intvl2` that have some
 overlap, and calculate their intersection as `intvl1_intersect_intvl2`.
 ......................................................................*)
 
-let intvl1 = failwith "not implemented" ;;
-let intvl2 = failwith "not implemented" ;;
-let intvl1_intersect_intvl2 = failwith "not implemented" ;;
-
+let intvl1 = IntInterval.create 3 8 ;;
+let intvl2 = IntInterval.create 4 10 ;;
+let intvl1_intersect_intvl2 = IntInterval.intersect intvl1 intvl2 ;;
 (* There's currently a problem with the `MakeInterval` functor. It's
 not abstract enough. Notably we are working with an invariant that a
 valid non-empty interval has an upper bound that is greater than or
@@ -163,9 +175,12 @@ an interval.
 
 module type INTERVAL = 
   sig 
-    type interval
-    type endpoint
-    (* ... complete the interface here ... *)
+    type interval    
+    type endpoint    
+    val create : endpoint -> endpoint -> interval    
+    val is_empty : interval -> bool    
+    val contains : interval -> endpoint -> bool    
+    val intersect : interval -> interval -> interval
   end ;;
 
 (*......................................................................
@@ -177,7 +192,42 @@ MakeInterval above.) **Don't forget to specify the module type.**
 
 module MakeSafeInterval (Endpoint : ORDERED_TYPE) =
   struct
-    (* ... complete the module implementation here ... *)
+type interval =
+      | Interval of Endpoint.t * Endpoint.t
+      | Empty
+
+    (* create low high -- Returns a new interval covering `low` to
+       `high` inclusive. If `low` is greater than `high`, then the
+       interval is empty. *)
+    let create (low : Endpoint.t) (high : Endpoint.t) : interval =
+      if Endpoint.compare low high > 0 then Empty
+      else Interval(low, high)
+
+    (* is_empty intvl -- Returns true if and only if `intvl` is
+       empty *)
+    let is_empty (intvl : interval) : bool =
+      match intvl with
+        | Empty -> true      
+        | Interval _ -> false
+
+    (* contains intvl x -- Returns true if and only if the value `x`
+       is contained within `intvl` *)
+    let contains (intvl : interval) (x : Endpoint.t) : bool =
+      match intvl with
+      | Empty -> false
+      | Interval (low, high) ->
+         Endpoint.compare x low >= 0 && Endpoint.compare x high <= 0
+
+    (* intersect intvl1 intvl2 -- Returns the intersection of `intvl1`
+       and `intvl2` *)
+    let intersect (intvl1 : interval) (intvl2 : interval) : interval =
+      let ordered x y = if Endpoint.compare x y <= 0 then x, y else y, x in
+        match intvl1, intvl2 with      
+        | Empty, _      
+        | _, Empty -> Empty      
+        | Interval (low1, high1), Interval (low2, high2) ->
+          let (_, low), (high, _)  = ordered low1 low2, ordered high1 high2 in
+             create low high
   end ;;
 
 (* We have successfully made our returned module abstract, but believe
@@ -190,7 +240,7 @@ Exercise 2C: Create an IntSafeInterval module using the new
 MakeSafeInterval functor.
 ......................................................................*)
 
-module IntSafeInterval = struct end ;;
+module IntSafeInterval =  MakeSafeInterval    (struct       type t = int       let compare = Stdlib.compare    end) ;;
 
 (* Now, try evaluating the following expression in the REPL:
 
@@ -239,6 +289,47 @@ to allow the creation of generic interval modules*.
 
 (* ... place your implementation of the MakeBestInterval functor here ... *)
 
+module MakeBestInterval (Endpoint : ORDERED_TYPE)                      : (INTERVAL with type endpoint = Endpoint.t) = 
+  struct
+type interval =
+      | Interval of Endpoint.t * Endpoint.t
+      | Empty
+
+    (* create low high -- Returns a new interval covering `low` to
+       `high` inclusive. If `low` is greater than `high`, then the
+       interval is empty. *)
+    let create (low : Endpoint.t) (high : Endpoint.t) : interval =
+      if Endpoint.compare low high > 0 then Empty
+      else Interval(low, high)
+
+    (* is_empty intvl -- Returns true if and only if `intvl` is
+       empty *)
+    let is_empty (intvl : interval) : bool =
+      match intvl with
+        | Empty -> true      
+        | Interval _ -> false
+
+    (* contains intvl x -- Returns true if and only if the value `x`
+       is contained within `intvl` *)
+    let contains (intvl : interval) (x : Endpoint.t) : bool =
+      match intvl with
+      | Empty -> false
+      | Interval (low, high) ->
+         Endpoint.compare x low >= 0 && Endpoint.compare x high <= 0
+
+    (* intersect intvl1 intvl2 -- Returns the intersection of `intvl1`
+       and `intvl2` *)
+    let intersect (intvl1 : interval) (intvl2 : interval) : interval =
+      let ordered x y = if Endpoint.compare x y <= 0 then x, y else y, x in
+        match intvl1, intvl2 with      
+        | Empty, _      
+        | _, Empty -> Empty      
+        | Interval (low1, high1), Interval (low2, high2) ->
+          let (_, low), (high, _)  = ordered low1 low2, ordered high1 high2 in
+             create low high
+  end ;;
+
+
 (* We now have a fully functioning functor that can create interval
 modules of whatever type we want, with the appropriate abstraction
 level.
@@ -259,4 +350,4 @@ instead?
     IntBestInterval.is_empty (IntBestInterval.Interval (4, 3)) ;;
 ......................................................................*)
 
-module IntBestInterval = struct end ;;
+module IntBestInterval =  MakeBestInterval    (struct       type t = int       let compare = Stdlib.compare    end) ;;
--- lab8_part2.ml
+++ lab8_part2.ml
@@ -102,29 +102,28 @@ module MakeStack (Element: SERIALIZE) : (STACK with type element = Element.t) =
 
     let empty : stack = []
 
-    let push (el : element) (s : stack) : stack =
-      failwith "push not implemented"
+    let push (el : element) (s : stack) : stack = el :: s
 
     let pop_helper (s : stack) : (element * stack) =
-      failwith "pop_helper not implemented"
+match s with      | [] -> raise Empty      | h :: t -> (h, t)
 
     let top (s : stack) : element =
-      failwith "top not implemented"
+      fst (pop_helper s)
 
     let pop (s : stack) : stack =
-      failwith "pop not implemented"
+      snd (pop_helper s)
 
     let map (f : element -> element) (s : stack) : stack =
-      failwith "map not implemented"
+      List.map
 
     let filter (f : element -> bool) (s : stack) : stack =
-      failwith "filter not implemented"
+      List.filter
 
     let fold_left (f : 'a -> element -> 'a) (init : 'a) (s : stack) : 'a =
-      failwith "fold_left not implemented"
+      List.fold_left
 
     let serialize (s : stack) : string =
-      failwith "serialize not implemented"
+      let string_join x y = Element.serialize y                  ^ (if x <> "" then ":" ^ x else "") in      fold_left string_join "" s
   end ;;
 
 (*......................................................................
@@ -132,8 +131,9 @@ Exercise 1B: Now, make a module `IntStack` by applying the functor
 that you just defined to an appropriate module for serializing integers.
 ......................................................................*)
 
-module IntStack = struct end ;;
+module IntSerialize : (SERIALIZE with type t = int) =  struct    type t = int    let serialize = string_of_int  end ;;
 
+module IntStack : (STACK with type element = IntSerialize.t) =  MakeStack(IntSerialize) ;;
 (*......................................................................
 Exercise 1C: Make a module `IntStringStack` that creates a stack whose
 elements are `int * string` pairs. Its serialize function should output
@@ -150,6 +150,7 @@ For this oversimplified serialization function, you may assume that
 the string will be made up of alphanumeric characters only.
 ......................................................................*)
 
-module IntStringStack = struct end ;;
+module IntStringSerialize =  struct    type t = (int * string)    let serialize (n, s) =      "(" ^ string_of_int n ^ ",'" ^ s ^ "')"  end ;;
+module IntStringStack =  MakeStack(IntStringSerialize) ;;
 
 
